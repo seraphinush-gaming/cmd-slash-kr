d149c7b67e8d95ef3fe1cb76e9e2b98d6fff302a
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-01-01 18:25:20 +0900

    Update index.js -- 1.14 Updated name

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // OPCODE REQUIRED :
 // - C_DISMISS_PARTY
-// - C_RESET_ALL_DUNGEON
 // - C_LEAVE_PARTY
+// - C_REQUEST_USER_PAPERDOLL_INFO
+// - C_RESET_ALL_DUNGEON
 
-// Version 1.13 r:00
+// Version 1.14 r:00
 
-module.exports = function shortSlashCommand(d) {
+module.exports = function CommandSlashKr(d) {
 	
 	// command
 	try {
@@ -25,7 +26,7 @@ module.exports = function shortSlashCommand(d) {
 			send(`Disbanded party.`.clr('56B4E9'))
 		})
 		// KR
-		command.add(['ㄹㅅ', 'ㄽ', 'ft', 'ㄹㄹ', 'ff'], () => {
+		command.add(['ㄹㄹ', 'ff'], () => { // 'ㄹㅅ', 'ㄽ', 'ft', 
 			d.toServer('C_RESET_ALL_DUNGEON', {})
 			send(`던전이 초기화 되었습니다.`.clr('56B4E9'))
 		})
@@ -33,12 +34,16 @@ module.exports = function shortSlashCommand(d) {
 			d.toServer('C_LEAVE_PARTY', {})
 			send(`파티 탈퇴했습니다.`.clr('56B4E9'))
 		})
-		command.add(['해산', 'ㅎㅅ', 'gy'], () => {
+		/*command.add(['해산', 'ㅎㅅ', 'gy'], () => {
 			d.toServer('C_DISMISS_PARTY', {})
 			send(`파티 해산됬습니다.`.clr('56B4E9'))
+		})*/
+		command.add(['보기', 'ㅂㄱ', 'qr'], (name) => {
+			if (name) d.toServer('C_REQUEST_USER_PAPERDOLL_INFO', { name: name })
+			else send(`Invalid argument.`.clr('FF0000'))
 		})
-		function send(msg) { command.message(`[short-slash-command] : ` + [...arguments].join('\n\t - ')) }
-	} catch (e) { console.log(`[ERROR] -- short-slash-command module --`) }	
+		function send(msg) { command.message(`[command-slash-kr] : ` + [...arguments].join('\n\t - ')) }
+	} catch (e) { console.log(`[ERROR] -- command-slash-kr module --`) }	
 	
 }
 
