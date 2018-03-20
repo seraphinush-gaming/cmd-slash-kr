97cf49ca36fd2557ea85aa5c12bad3b4ab1e69dd
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-03-20 23:30:49 +0900

    1.16 r:00

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
-// OPCODE REQUIRED :
-// - C_DISMISS_PARTY
-// - C_LEAVE_PARTY
-// - C_RESET_ALL_DUNGEON
+// Version 1.16 r:00
 
-// Version 1.15 r:00
-
-module.exports = function CmdSlashKr(d) {
-	
-	// command
-	try {
-		const Command = require('command')
-		const command = Command(d)
-		// NA
-		command.add(['res', 'ㄱㄷㄴ', 'ㄹㄹ', 'ff'], () => {
-			d.toServer('C_RESET_ALL_DUNGEON', {})
-			send(`Dungeons reset.`.clr('56B4E9'))
-		})
-		command.add(['dr', 'ㅇㄱ', 'ㅌㅌ', 'xx'], () => {
-			d.toServer('C_LEAVE_PARTY', {})
-			send(`Dropped party.`.clr('56B4E9'))
-		})
-		command.add(['dis', '얀'], () => {
-			d.toServer('C_DISMISS_PARTY', {})
-			send(`Disbanded party.`.clr('56B4E9'))
-		})
-		function send(msg) { command.message(`[cmd-slash-kr] : ` + [...arguments].join('\n\t - ')) }
-	} catch (e) { console.log(`[ERROR] -- cmd-slash-kr module --`) }
-	
-}
+const Command = require('command')
 
 // credit : https://github.com/Some-AV-Popo
 String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}` }
+
+module.exports = function CmdSlashKr(d) {
+	const command = Command(d)
+
+	// command
+	command.add(['res', 'ㄱㄷㄴ', 'ㄹㄹ', 'ff'], () => {
+		d.toServer('C_RESET_ALL_DUNGEON', {})
+		send(`Dungeons reset.`.clr('56B4E9'))
+	})
+	command.add(['dr', 'ㅇㄱ', 'ㅌㅌ', 'xx'], () => {
+		d.toServer('C_LEAVE_PARTY', {})
+		send(`Dropped party.`.clr('56B4E9'))
+	})
+	function send(msg) { command.message(`[cmd-slash-kr] : ` + msg) }
+
+}
